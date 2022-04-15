d1e86ee58a24e1a86d9647bf7d3eab8092a6a712
robertomdiniz5 <robertomdiniz@protonmail.com>
2022-04-15 17:08:11 -0300

    ok

---
+++ cronFolder/pip.sh
@@ -0,0 +1,2 @@
+pip install plotly
+pip install --upgrade nbformat
--- cronFolder/vizualizing.py
+++ cronFolder/vizualizing.py
@@ -1,14 +1,14 @@
 #%%
+from datetime import datetime
 from email import header
 from regex import D
 from tabulate import tabulate
 import pandas as pd
-from matplotlib import pyplot as plt
-
+import plotly.express as px
 #%%
 folderName = 'dataset_speedtest'
 excelFile = folderName + '/speedTest.xlsx'
-df = pd.read_excel(excelFile, parse_dates=True)
+df = pd.read_excel(excelFile)
 #%%
 print(df.columns)
 
@@ -19,11 +19,18 @@ df = df [lstCln]
 show = tabulate(df, headers='keys',tablefmt="grid")
 print(show)
 #%%
+df['Timestamp'] = pd.to_datetime(df['Timestamp'],infer_datetime_format=True)
+df['Download'] = df['Download']/ (100**3)
+
+#%% 
+
 dw =df['Download']
 tm =df['Timestamp']
-#%%
+
+fig = px.scatter(x=tm, y=dw)
+fig.show()
 
 
-plt.plot(tm, dw)
-plt.show()
+# %%
 
+# %%
