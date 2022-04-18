8909df133cafd953f2498a9e360fbf1c18d9c191
Roberto M. Diniz <78979759+s33ding@users.noreply.github.com>
2022-04-18 15:10:36 -0300

    Update creatingDataset.py

--- creatingDataset.py
+++ creatingDataset.py
@@ -3,9 +3,7 @@
 import pandas as pd 
 #%%
 lst_columns = 'Server ID,Sponsor,Server Name,Timestamp,Distance,Ping,Download,Upload,Share,IP Address'.split(",")
-print(lst_columns)
 #%%
 df = pd.DataFrame( columns=lst_columns)
-
 df.to_csv('dataset_speedtest/speedTest.csv')
 #%%
