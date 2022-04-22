a5a1877bbb195f28f387938ac9718b37e0853a88
s33ding <robertomdiniz@protonmail.com>
2022-04-22 17:46:40 -0300

    ok

--- mainFile copy.py
+++
@@ -1,6 +0,0 @@
-# !rm -r dataset_speedtest/main.db
-import os 
-os.system("bash speedTest.sh")
-os.system("python3 creatingDataset.py")
-os.system("python3 populatingSpeedtest.py")
-os.system("python3 tabulateSpeedtest.py")
