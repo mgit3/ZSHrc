584fcd3abb951ad188eaa94f68028aa0e340b21b
s33ding <robertomdiniz@protonmail.com>
2022-04-23 19:37:13 -0300

    ok

--- dash_plotly.py
+++ dash_plotly.py
@@ -1,11 +1,15 @@
 #%%
+from click import style
 from sqlalchemy import create_engine
 from sqlalchemy import MetaData 
 from sqlalchemy import Table
 import pandas as pd
-import plotly.express as px
 import dash 
 import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+from datetime import datetime as dt
+
 #%%
 tableName ='speedtest' 
 engine = create_engine("sqlite:///dataset_speedtest/main.db")
@@ -16,18 +20,26 @@ fig_scatter_dw_up = px.scatter(df,
             x=df.Timestamp, 
             y=[df.Download,df.Upload], 
             labels={'x':'Timestamp', 'y':['Download','Upload']},
-            title="Total Sales by Country and Month"
+            title="Download and Upload Speed"
             )
 #%%
 
 # Create the Dash app
 app = dash.Dash(__name__)
 
+author = "Roberto"; lastName = "Moreira Diniz"; fullName = f"{author} {lastName}"; profession = "Data Engineer"
+dashTitle = f"{author}'s Dashboard"  
+
+
 # Set up the layout with a single graph
-app.layout = dcc.Graph(
-  id='download_and_upload_speed',
-  # Insert the line graph
-  figure=fig_scatter_dw_up)
+app.layout = html.Div(children=[
+    html.Div(style={'width':1000,'height':10,'background-color':'darkblue'}),
+    html.H1(dashTitle),
+    html.Br(),
+    html.Span(children=[f"Prepared: {dt.now().date()} by {fullName}, {profession}."]),
+    dcc.Graph(id='scatter_dwUp',figure=fig_scatter_dw_up)
+    ]
+)
 
 # Set the app to run in development mode
 if __name__ == '__main__':
