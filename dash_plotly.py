a5a2d94a2340596467cd671e9a6ca439454abfca
s33ding <robertomdiniz@protonmail.com>
2022-04-23 23:23:59 -0300

    ok

--- dash_plotly.py
+++ dash_plotly.py
@@ -8,6 +8,7 @@ import pandas as pd
 import dash 
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_table
 import plotly.express as px
 from datetime import datetime as dt
 
@@ -17,58 +18,80 @@ engine = create_engine("sqlite:///dataset_speedtest/main.db")
 query = "select * from speedtest"; df = pd.read_sql(query,engine)
 #%%
 #using the plotly modelu to produce a graph
+graphColor = 'rgb(240, 245, 245)'
 fig_scatter_dw_up = px.scatter(df,
             x=df.Timestamp, 
             y=[df.Download,df.Upload], 
             labels={'x':'Timestamp', 'y':['Download','Upload']},
-            title="Download and Upload Speed"
-            )
+            title="Download and Upload Speed").update_layout(paper_bgcolor=graphColor)
 #%%
 
 # Create the Dash app
 app = dash.Dash(__name__)
 
-author = "Roberto"; lastName = "Moreira Diniz"; fullName = f"{author} {lastName}"; profession = "Data Engineer"
-dashTitle = f"{author}'s Dashboard"  
-logo_link = 'https://seeklogo.com/images/E/endless_knot-logo-1A4534EFCF-seeklogo.com.png'
-myColor =   'rgb(54, 69, 99)'
-textColor =   'white'
+author ="Roberto"; lastName = "Moreira Diniz"; fullName = f"{author} {lastName}"; profession = "Data Engineer"
+dashTitle =f"{author}'s Dashboard"  
+logo_link ='https://seeklogo.com/images/E/endless_knot-logo-1A4534EFCF-seeklogo.com.png'
+myColor ='rgb(15, 33, 62)'
+textColor ='white'
+tableHeaderColor = 'rgb(91, 138, 215)'
 
 app.layout = html.Div(
   children=[
     html.Br(),
     html.Div(style={'width':10000,'height':10,'background-color':'white'}),
-    html.H1(dashTitle),
-    html.Br(),
-    html.Span(children=[f"Prepared: {dt.now().date()} by {fullName} {profession}."]),
-    html.Br(),html.Br(),
-    dcc.Graph(id='scatter_dwUp',figure=fig_scatter_dw_up,style={'width':'900px', 'margin':'auto', 'background-color': myColor}),
-    html.Br(),
     
+    html.H1(dashTitle),
+    html.Span(children=[f"Prepared: {dt.now().date()} by {fullName} {profession}."]),
+    html.Br(),
+
+    html.Span(children=[
+        
+    dcc.Graph(id='scatter_dwUp',figure=fig_scatter_dw_up,
+    style={
+    'width':'900px', 
+    'margin':'auto', 
+    }),html.Br()],style={'color':textColor, 'display':'inline-block','margin':'100px'}),
     
     html.Span(children=[
-    html.B('HIGHEST:'),
-    html.Ol(children=[
+
+    html.Ul(children=[
+        html.B('HIGHEST:'),
       	# Add two list elements with the top category variables
         html.Li(children=[f"Download speed - {round(df['Download'].max(),2)} Mb/s."]),
         html.Li(children=[f"Upload speed - {round(df['Upload'].max(),2)} Mb/s."]),
-        ],style={'width':'350px', 'margin':'auto'}),
-    html.Br(),
-    ]),
+        ],style={'width':'350px'}),html.Br(),
 
-    html.Span(children=[
-    html.B('LOWEST:'),
-    html.Ol(children=[
+    html.Ul(children=[
+        html.B('MIDDLE:'),
+      	# Add two list elements with the top category variables
+        html.Li(children=[f"Download speed - {round(df['Download'].mean(),2)} Mb/s."]),
+        html.Li(children=[f"Upload speed - {round(df['Upload'].mean(),2)} Mb/s."]),
+        ],style={'width':'350px'}),html.Br(),
+
+    html.Ul(children=[
+        html.B('LOWEST:'),
       	# Add two list elements with the top category variables
         html.Li(children=[f"Download speed - {round(df['Download'].min(),2)} Mb/s."]),
         html.Li(children=[f"Upload speed - {round(df['Upload'].min(),2)} Mb/s."]),
-        ],style={'width':'350px', 'margin':'auto'}),
-    html.Br(),
-    ]),
+        ],style={'width':'350px'}),html.Br(),
+    
+    ],style={'text-align':'top','display':'inline-block', 'margin':'10px 10px 30px 100px'}), 
+
+    dash_table.DataTable(
+      id='table',
+      data = df.to_dict('records'),
+      style_cell={'padding': '5px','backgroundColor':myColor},
+      style_header={'backgroundColor':tableHeaderColor,'fontWeight': 'bold'},
+      ),
 
     html.Div(style={'width':10000,'height':10,'background-color':'white'}),
     html.Br(),
-  ],style={'text-align':'center', 'font-size':22, 'background-color':myColor, 'color':textColor}
+  ],style={
+    'text-align':'center', 
+    'font-size':22, 
+    'background-color':myColor, 
+    'color':textColor},
 )
 
 # Set the app to run in development mode
