14c2a1cf8b7af700b297d0782bab38b31904c678
s33ding <robertomdiniz@protonmail.com>
2023-03-08 23:20:27 +0000

    saving

--- linux_server/dash-docker/app/app.py
+++ linux_server/dash-docker/app/app.py
@@ -8,24 +8,26 @@ from click import style
 from matplotlib.pyplot import margins
 from datetime import datetime as dt
 import dash
-import dash_table
-from dash import dcc
-from dash import html
-
 import plotly.graph_objects as go
+
+
+import dash_table
+import dash_core_components as dcc
+import dash_html_components as html
 import plotly.express as px
+from flask import Flask
 
 def get_df():
     # Get data from Redis
     data = get_speedtest_data_from_redis()
-    print(data)
     # Create a Pandas dataframe from the data
     df = pd.DataFrame.from_dict(data['speedtest_data'], orient='index', columns=['download', 'upload'])
     df.reset_index(inplace=True)
     df.rename(columns={'index': 'datetime'}, inplace=True)
+    print(df)
     return df
 
-df = get_df() 
+df = get_df()
 
 #using the plotly modelu to produce a graph
 graphColor = 'rgb(240, 245, 245)'
@@ -47,12 +49,15 @@ fig_boxPlot.add_trace(go.Box(x=df['upload'],  name='UPLOAD',marker_color = redPl
 
 
 # Create the Dash app
-app = dash.Dash(__name__)
+server = Flask(__name__)
+
+app = dash.Dash(server=server)
+server=server
 
 
 #transforming the graph into html
 author ="Roberto"; lastName = "Moreira Diniz"; fullName = f"{author} {lastName}"; profession = "Data Engineer"
-dashTitle =f"{author}'s Dashboard"  
+dashTitle =f"{author}'s Dashboard"
 myColor ='#282a36'
 textColor ='white'
 tableHeaderColor = '#44475a'
@@ -65,7 +70,7 @@ app.layout = html.Div(
     html.Span(children=[f"Prepared: {dt.now().date()} by {fullName}, {profession}."]),
     html.Br(),
 
-        
+
 html.Div(children=[
     dcc.Graph(id='scatter_dwUp',figure=fig_scatter_dw_up,
     style={'margin':'auto','height':'400px'})], style={'color': textColor, 'margin': marginSize}),
@@ -74,8 +79,8 @@ html.Div(children=[
 
       html.P(children=[f"Last download Rate: {round(df['download'].iloc[-1],2)} Mb/s."],style={'width':'350px','display':'inline-block'}),
       html.P(children=[f"Last upload Rate: {round(df['upload'].iloc[-1],2)} Mb/s."],style={'width':'350px','display':'inline-block'}),
-    
-    ],style={'font-size':'18px','display':'inline-block', 'margin':'0px auto'}), 
+
+    ],style={'font-size':'18px','display':'inline-block', 'margin':'0px auto'}),
 
 
 html.Div(children=[
@@ -100,49 +105,49 @@ html.Div(children=[
     dcc.Graph(id='boxPLot',figure=fig_boxPlot,
       style={
       'width':'1260px',
-      'height':'460px', 
-      'margin':'auto', 
+      'height':'460px',
+      'margin':'auto',
       }),html.Br()],style={'color':textColor, 'display':'inline-block','padding':'200px auto'}),
-    
-    
+
+
     html.Span(children=[
 
     html.Ul(children=[
         html.B('HIGHEST:'),
-      	# Add two list elements with the top category variables
+        # Add two list elements with the top category variables
         html.Li(children=[f"download speed - {round(df['download'].max(),2)} Mb/s."]),
         html.Li(children=[f"upload speed - {round(df['upload'].max(),2)} Mb/s."]),
         ],style={'width':'350px'}),html.Br(),
 
     html.Ul(children=[
         html.B('AVERAGE:'),
-      	# Add two list elements with the top category variables
+        # Add two list elements with the top category variables
         html.Li(children=[f"download speed - {round(df['download'].mean(),2)} Mb/s."]),
         html.Li(children=[f"upload speed - {round(df['upload'].mean(),2)} Mb/s."]),
         ],style={'width':'350px'}),html.Br(),
 
     html.Ul(children=[
         html.B('LOWEST:'),
-      	# Add two list elements with the top category variables
+        # Add two list elements with the top category variables
         html.Li(children=[f"download speed - {round(df['download'].min(),2)} Mb/s."]),
         html.Li(children=[f"upload speed - {round(df['upload'].min(),2)} Mb/s."]),
         ],style={'width':'350px'}),html.Br(),
 
     html.Ul(children=[
         html.B('STANDARD DESVIATION:'),
-      	# Add two list elements with the top category variables
+        # Add two list elements with the top category variables
         html.Li(children=[f"download speed - {round(df['download'].std(),2)} Mb/s."]),
         html.Li(children=[f"upload speed - {round(df['upload'].std(),2)} Mb/s."]),
         ],style={'width':'350px'}),html.Br(),
-    
-    ],style={'font-size':'18px','display':'inline-block', 'margin':f'50px 50px 50px {marginSize}'}), 
+
+    ],style={'font-size':'18px','display':'inline-block', 'margin':f'50px 50px 50px {marginSize}'}),
 
 
     html.Br(),
   ],style={
-    'text-align':'center', 
-    'font-size':22, 
-    'background-color':myColor, 
+    'text-align':'center',
+    'font-size':22,
+    'background-color':myColor,
     'color':textColor},
 )
 
