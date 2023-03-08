102a058343bda3423ab40a09e35bae39155ad28a
s33ding <robertomdiniz@protonmail.com>
2023-03-08 23:21:15 +0000

    saving

--- linux_server/script/pop.py
+++
@@ -1,8 +0,0 @@
-from useful_fuctions import * 
-
-all_data = get_speedtest_data_from_redis()
-speedtest_data = run_speedtest()
-key = speedtest_data["datetime"]
-del speedtest_data["datetime"]
-all_data["speedtest_data"][key]=speedtest_data
-store_speedtest_data_in_redis(all_data)
